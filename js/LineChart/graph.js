a0a86669de1dd2770d3963006b24c904f6a49e39
AkiraTorres <torres.thg@gmail.com>
2022-08-09 06:41:41 -0300

    FEAT: Basic grafic with mocked data. Need improving and code refactoring.

--- js/LineChart/graph.js
+++ js/LineChart/graph.js
@@ -1,15 +1,6 @@
 function convertDate(epoch_date) {
     return new Date(+epoch_date * 1000);
-}    
-
-// CUIDADO
-/*function getEvent(component, action, target) {
-    d3.csv("./data/event_mapping.csv", function(data) {
-        if(((component == data.component) && (action == data.action) && target == (data.target))) {
-            return data.event;
-        }
-    })
-}*/
+}
 
 usersData = []
 
@@ -18,81 +9,27 @@ d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv",
     function (data) {
 
         usersData.push(
-            {   id: data.userid, 
+            {
+                id: data.userid,
                 time: convertDate(data.t),
                 //timeLast: (d3.timeParse("%Y-%m-%d")(convertDate(data.t))), // Não Funciona
                 //event: getEvent(data.component, data.action, data.target),  // CUIDADO
-                component: data.component, 
-                action: data.action, 
-                target: data.target
-
-
+                component: data.component,
+                action: data.action,
+                target: data.target,
             });
 
     }).then(function () {
         usersData.forEach(function (d) {
-            console.log(d)
-    })
-})  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            // console.log(d)
+        })
+    });
 
 // Static Graph
 
-const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 460 - margin.left - margin.right,
+const margin = { top: 10, right: 5, bottom: 30, left: 90 },
+    width = 860 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
-
 
 const svg = d3.select("#canvas")
     .append("svg")
@@ -101,62 +38,45 @@ const svg = d3.select("#canvas")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-d3.csv("./data/graphData.csv",
+d3.csv("./data/graphData.csv", (d) => {
+    return { date: d.date, value: d.value }
+}).then((data) => {
+    // const x = d3.scaleTime()
+    // .domain(d3.extent(data, d => d.date))
+    // .range([0, width]);
+    const x = d3.scaleBand()
+        .domain(data.map(function (d) { return d.date; }))
+        .range([0, width])
+        .padding(1);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    function (d) {
-        return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value }
-    }).then(
+    // const y = d3.scaleLinear()
+    const y = d3.scaleBand()
+        .domain(data.map(d => d.value))
+        .range([height, 0])
+        .padding(1);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-        function (data) {
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "#69b3a2")
+        .attr("stroke-width", 2)
+        .attr("d", d3.line()
+            .x(d => x(d.date))
+            .y(d => y(d.value))
+        )
 
-            const x = d3.scaleTime()
-                .domain(d3.extent(data, d => d.date))
-                .range([0, width]);
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-
-            const y = d3.scaleLinear()
-                .domain([8000, 9200])
-                .range([height, 0]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
-
-            svg.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke", "#69b3a2")
-                .attr("stroke-width", 1.5)
-                .attr("d", d3.line()
-                    .x(d => x(d.date))
-                    .y(d => y(d.value))
-                )
-
-            svg
-                .append("g")
-                .selectAll("dot")
-                .data(data)
-                .join("circle")
-                .attr("cx", d => x(d.date))
-                .attr("cy", d => y(d.value))
-                .attr("r", 5)
-                .attr("fill", "#69b3a2")
-        })
-
-
-/*const DATASTORE = {
-    curse_infos: null,
-    event_mapping: null,
-}
-
-d3.csv("./data/event_mapping.csv",
-    function (data) {
-        DATASTORE.event_mapping = data;
-    });
-
-d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv", 
-    function (data) {
-        DATASTORE.curse_infos = data;
-    })
-
-console.log(DATASTORE); */
+    svg
+        .append("g")
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+        .attr("cx", d => x(d.date))
+        .attr("cy", d => y(d.value))
+        .attr("r", 5)
+        .attr("fill", "#69b3a2")
+});
