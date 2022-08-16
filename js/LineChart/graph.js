a908a8de0efc61899e6a28ce0c9893e13f04c854
Júlio Cesar <juliocesar27072004@outlook.com>
2022-08-16 05:41:05 -0300

    Update graph.js

--- js/LineChart/graph.js
+++ js/LineChart/graph.js
@@ -4,42 +4,25 @@ export default (user) => {
         return new Date(+epoch_date * 1000);
     }
 
-    function filterByPeriod(data, period) {
-        if (data.t >= period.start && data.t <= period.end) {
+    function filterByPeriod(rawData) {
+
+        const timeStart = 1573596966
+        const timeEnd = 1574207999
+
+        if (rawData.t >= timeStart && rawData.t <= timeEnd) {
             return true;
         }
     }
 
-    let userData = [];
-
-    d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv",
-        function (data) {
-            if (filterByPeriod(data, {start: 1573596966, end: 1574207999 })) { // 1 Week 
-                if (data.userid == user.id) {
-                    d3.csv("./data/event_mapping.csv", function (event) {
-                        if(data.component == event.component && data.action == event.action && data.target == event.target){
-                            
-                            let count = 0 
-                            userData.forEach(function (d) { // Check if the event already exists in the array
-                                if(d.event == event.class){
-                                    count++;
-                                }
-                            })
-
-                            if(count == 0) {
-                                userData.push({
-                                    date: d3.timeFormat("%A, %d")(convertDate(data.t)),
-                                    event: event.class
-                                });
-                            }
-                        }
-                    })
-                }
-            }
+    function filterByUser(rawData) {
+        if (rawData.userid === user.id) {
+            return true;
         }
-    )
+    }
 
-    console.log(userData);
+    function sortEventsByTime(sequence) {
+        return sequence.sort((a, b) => +a.t - +b.t);
+    }
 
     // Static Graph
     d3.select("svg").remove();
@@ -55,46 +38,82 @@ export default (user) => {
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    d3.csv("./data/graphData.csv", (d) => {
-        return { date: d.date, value: d.value }
-    }).then((data) => {
-        // const x = d3.scaleTime()
-        // .domain(d3.extent(data, d => d.date))
-        // .range([0, width]);
-        const x = d3.scaleBand()
-            .domain(data.map(function (d) { return d.date; }))
-            .range([0, width])
-            .padding(1);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+    d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv")
+        .then(function (datastore) {
 
-        // const y = d3.scaleLinear()
-        const y = d3.scaleBand()
-            .domain(data.map(d => d.value))
-            .range([height, 0])
-            .padding(1);
-        svg.append("g")
-            .call(d3.axisLeft(y));
+            sortEventsByTime(datastore);
 
-        svg.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 2)
-            .attr("d", d3.line()
-                .x(d => x(d.date))
-                .y(d => y(d.value))
+            var userData = []
+            var auxEventList = []
+
+            datastore.forEach(function (rawData) {
+                if (filterByPeriod(rawData) && filterByUser(rawData)) {
+                    d3.csv("./data/event_mapping.csv", function (event) {
+                        if (rawData.component == event.component && rawData.action == event.action && rawData.target == event.target) {
+
+                            if (auxEventList.length == 0) {
+                                auxEventList.push(event.class)
+                                userData.push({
+                                    date: d3.timeFormat("%A, %d")(convertDate(rawData.t)),
+                                    event: event.class
+                                })
+                            } else {
+                                if (!auxEventList.includes(event.class)) {
+                                    auxEventList.push(event.class)
+                                    userData.push({
+                                        date: d3.timeFormat("%A, %d")(convertDate(rawData.t)),
+                                        event: event.class
+                                    })
+                                }
+                            }
+
+                            return userData
+
+                        }
+                    }).then(function (data) {
+
+                      
+                            const x = d3.scaleBand()
+                                .domain(data[0].map(function (d) { return d.date; }))
+                                .range([0, width])
+                                .padding(1);
+                            svg.append("g")
+                                .attr("transform", "translate(0," + height + ")")
+                                .call(d3.axisBottom(x));
+
+                            // const y = d3.scaleLinear()
+                            const y = d3.scaleBand()
+                                .domain(data[0].map(d => d.event))
+                                .range([height, 0])
+                                .padding(1);
+                            svg.append("g")
+                                .call(d3.axisLeft(y));
+
+                            svg.append("path")
+                                .datum(data[0])
+                                .attr("fill", "none")
+                                .attr("stroke", "#69b3a2")
+                                .attr("stroke-width", 2)
+                                .attr("d", d3.line()
+                                    .x(d => x(d.date))
+                                    .y(d => y(d.event))
+                                )
+
+                            svg
+                                .append("g")
+                                .selectAll("dot")
+                                .data(data[0])
+                                .join("circle")
+                                .attr("cx", d => x(d.date))
+                                .attr("cy", d => y(d.event))
+                                .attr("r", 5)
+                                .attr("fill", "#69b3a2")
+
+                    })
+                }
+            }
             )
+        })
 
-        svg
-            .append("g")
-            .selectAll("dot")
-            .data(data)
-            .join("circle")
-            .attr("cx", d => x(d.date))
-            .attr("cy", d => y(d.value))
-            .attr("r", 5)
-            .attr("fill", "#69b3a2")
-    });
+
 }
