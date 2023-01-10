9dc2efbb350bb745a0e871fa73747863292dcbed
Júlio Cesar <juliocesar27072004@outlook.com>
2023-01-10 16:10:59 -0300

    Delete js/Tools/DateFunction directory

--- js/Tools/DateFunction/date.js
+++
@@ -1,6 +0,0 @@
-//encapsulates date conversion from epoch, for future general adjustments (eg timezone)
-function epochToDate(epoch_date) {
-    return new Date(+epoch_date * 1000); //moodle epoch is in seconds, no milliseconds   
-}    
-
-export default epochToDate;
