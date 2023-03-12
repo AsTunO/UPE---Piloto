717878a9acf9c3d8d845a5c657f11c364f51dcd4
Júlio Cesar <juliocesar27072004@outlook.com>
2023-03-12 11:39:12 -0300

    Add files via upload

---
+++ js/BubbleChart/createGraph.js
@@ -0,0 +1,51 @@
+function createGraph(domainsContent, data) {
+
+    d3.select("svg").remove();
+
+    const margin = { top: 10, right: 5, bottom: 30, left: 90 },
+        width = 860 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("#canvas")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Draw X domain
+    const x = d3.scaleBand()
+        .domain((domainsContent.x).map(d => d))
+        .range([0, width])
+        .padding(1);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Draw Y domain
+    const y = d3.scaleBand().domain((domainsContent.y).map(d => d))
+        .range([height, 0])
+        .padding(1);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add a scale for bubble size
+    var z = d3.scaleLinear()
+        .domain([0, 100])
+        .range([1, 40]);
+
+    // Add dots
+    svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.date); })
+        .attr("cy", function (d) { return y(d.event); })
+        .attr("r", function (d) { return z(d.tot); })
+        .style("fill", "#69b3a2")
+        .style("opacity", "0.7")
+        .attr("stroke", "black")
+}
+
+export default createGraph
--- js/main.js
+++ js/main.js
@@ -1,96 +1,27 @@
-import createGraph from './LineChart/graph.js';
-import populateSelectStudents from './Tools/AuxFunctions/populateSelectStudents.js';
-import populateSelectFilters from './Tools/AuxFunctions/populateSelectFilters.js';
-import filterByPeriod from './Tools/FiltersFunctions/filterByPeriod.js';
-import getDomainsContent from './Tools/GetsFunctions/getDomainsContent.js'
-import sortEventByTime from './Tools/SortsFunctions/sortEventsByTime.js'
-import getUserData from './Tools/GetsFunctions/getUserData.js'
-import setUserGrade from './Tools/SetsFunction/setUserGrade.js'
-import getPeriod from './Tools/GetsFunctions/getPeriod.js'
-
-populateSelectStudents("./data/user_list_see.csv")
+import createGraph from "./BubbleChart/createGraph.js";
+import populateSelectFilters from "./Tools/AuxFunctions/populateSelectFilters.js";
+import filterLogsByPeriodAndActivity from "./Tools/FiltersFunctions/filterLogsByPeriodAndActivity.js"
 
 Promise.all([
-    d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv"),
-    d3.csv("./data/event_mapping.csv"),
-    d3.csv("./data/see_course2060_quiz_grades.csv"),
-    d3.csv("./data/see_course2060_quiz_list.csv")
+    d3.csv("./data/see_course2060_quiz_list.csv"),
+    d3.csv("./data/see_course2060_12-11_to_11-12_logs_filtered.csv")
 ]).then(function (data) {
 
-    const logs_filtered_data = data[0]
-    const event_mapping_data = data[1]
-    const logs_grades = data[2]
-    const quiz_list = data[3]
+    const quizList = data[0]
+    const logs = data[1]
 
-    let activities = populateSelectFilters(quiz_list)
+    const activities = populateSelectFilters(quizList)
+    //const graphData = filterLogsByPeriodAndActivity(logs, activities, quizList)
 
-    const activities_period = []
+
+    //const data_to_be_plotted = filterData(quiz_list, logs)
+
+    const defaultData = [{ date: "Tuesday, 19", event: "course_vis", tot: 10 }, { date: "Saturday, 23", event: "resource_vis", tot: 20 }, { date: "Monday, 25", event: "assignment_vis", tot: 30 }, { date: "Saturday, 23", event: "assignment_try", tot: 40 }, { date: "Friday, 22", event: "assignment_sub", tot: 50 }]
+    const domainContent = {x : ["Tuesday, 19","Wednesday, 20","Thursday, 21","Friday, 22","Saturday, 23","Sunday, 24","Monday, 25","Tuesday, 26"],
+                            y: ["course_vis", "resource_vis", "forum_vis", "forum_participation", "assignment_vis", "assignment_try", "assignment_sub"]}
     
-    activities.forEach((e) => {
-        e = e.text
-        let period = getPeriod(e, quiz_list)
-        activities_period.push({text : e, period : period})
-    })
+    createGraph(domainContent, defaultData)
 
-    const logs_filtered_by_period_and_activity = []
-
-    activities_period.forEach((e) => {
-        const logs_filtered_by_period = []
-        logs_filtered_data.forEach((d) => {
-            if (filterByPeriod(d, e.period)) {
-                logs_filtered_by_period.push(d)
-            }
-        })
-        sortEventByTime(logs_filtered_by_period)
-        logs_filtered_by_period_and_activity.push({activity : e.text, logs : logs_filtered_by_period})
-    })
-
-    // Default Student
-    let user = {
-        id: "239",
-        text: "Isabelle Santos"
-    }
-
-    let selected_options = {user : user, activity: "Atividade 1"}; 
-    let logs_filtered_by_period = logs_filtered_by_period_and_activity[0].logs
-    let userData = getUserData(logs_filtered_by_period, event_mapping_data,logs_grades, user)
-    let domainsContent = getDomainsContent(logs_filtered_by_period)
-    setUserGrade(userData[userData.length - 1])
-    createGraph(domainsContent, userData);
-
-    const tagGraph = document.getElementById('student-name-graph');
-    const tagGrade = document.getElementById('student-name-grade');
-    const selectorStudents = document.getElementById('students');
-    const selectorActivities = document.getElementById('activities');
-    let activity
-
-    function updateFields() {
-        tagGraph.textContent = selectorStudents.options[selectorStudents.selectedIndex].text;
-        tagGrade.textContent = selectorStudents.options[selectorStudents.selectedIndex].text;
-
-        user.id = selectorStudents.options[selectorStudents.selectedIndex].id
-        user.text = selectorStudents.options[selectorStudents.selectedIndex].text
-        activity = selectorActivities.options[selectorActivities.selectedIndex].text
-
-        selected_options.user = user
-        selected_options.activity = activity
-
-        let logs_filtered_by_period 
-
-        logs_filtered_by_period_and_activity.forEach((e) => {
-            if(activity == e.activity) {
-                logs_filtered_by_period = e.logs
-            }
-        })
-
-        domainsContent = getDomainsContent(logs_filtered_by_period)        
-        userData = getUserData(logs_filtered_by_period, event_mapping_data, logs_grades, user)
-        setUserGrade(userData[userData.length - 1])
-        createGraph(domainsContent, userData);
-        
-    }
-
-    selectorStudents.addEventListener('change', () => updateFields())
-    selectorActivities.addEventListener('change', () => updateFields())
-
+    /*const selectorActivities = document.getElementById('activities');
+    selectorActivities.addEventListener('change', () => {})*/
 })
