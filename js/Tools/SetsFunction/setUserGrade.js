e3365bcdf4c901bca7f5f9ccfce8a8be42b6a48e
Júlio Cesar <juliocesar27072004@outlook.com>
2023-04-16 12:27:17 -0300

    Delete js/Tools/SetsFunction directory

--- js/Tools/SetsFunction/setUserGrade.js
+++
@@ -1,8 +0,0 @@
-function setUserGrade(userGrade) {
-
-    const grade_text = document.getElementById('student-grade-content');
-    grade_text.textContent = userGrade.grade
-
-}
-
-export default setUserGrade
