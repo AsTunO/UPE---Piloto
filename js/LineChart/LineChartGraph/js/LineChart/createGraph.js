e68aa729263d3dd8990e7dc78b3360c7f0803db8
Júlio Cesar <juliocesar27072004@outlook.com>
2023-11-08 10:16:07 -0300

    Update createGraph.js

--- js/LineChart/LineChartGraph/js/LineChart/createGraph.js
+++ js/LineChart/LineChartGraph/js/LineChart/createGraph.js
@@ -5,7 +5,7 @@ function createGraph(data) {
     count++;
     if(count <= 3) {
 
-        const margin = { top: 0, right: 1, bottom: 30, left: 20 },
+        const margin = { top: 0, right: 1, bottom: 60, left: 20 },
             width = 1000,
             height = 200
     
@@ -16,15 +16,47 @@ function createGraph(data) {
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
     
-        // Draw X domain
+            const diasDaSemana = {
+                'Monday': 'Segunda-feira',
+                'Tuesday': 'Terça-feira',
+                'Wednesday': 'Quarta-feira',
+                'Thursday': 'Quinta-feira',
+                'Friday': 'Sexta-feira',
+                'Saturday': 'Sábado',
+                'Sunday': 'Domingo'
+              };
+    
+              
+    
         const x = d3.scaleBand()
-            .domain((data.domainContent.x).map(d => d))
-            .range([0, width])
-            .padding(1);
-        
-        svgLine.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+        .domain(data.domainContent.x)
+        .range([0, width])
+        .padding(1);
+    
+    svgLine.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Adicione traduções em português inclinadas abaixo do eixo X
+svgLine.append("g")
+.selectAll("text")
+.data(data.domainContent.x)
+.enter()
+.append("text")
+.text(d => {
+    const diaSemana = d.split(",")[0]; // Extrai o dia da semana
+    return `${diasDaSemana[diaSemana]}, ${d.split(",")[1]}`; // Combina a tradução com o resto da data
+})
+.attr("x", d => x(d) + x.bandwidth() / 2)
+.attr("y", height + 30) // Ajuste a posição vertical conforme necessário
+.attr("text-anchor", "middle")
+.attr("font-size", 10) // Ajuste o tamanho da fonte conforme necessário
+.attr("fill-opacity", 1) // Define o preenchimento para tornar o texto visível
+.attr("transform", d => `rotate(-25, ${x(d) + x.bandwidth() / 2}, ${height + 80})`); // Inclina o texto em -45 graus
+
+// Estilize os elementos de texto originais em inglês como transparentes
+svgLine.selectAll(".tick text")
+.attr("fill-opacity", 0);
     
         // Draw Y domain
         const y = d3.scaleBand()
