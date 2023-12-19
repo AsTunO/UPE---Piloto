189f68f777d92e547d76fab4ea886d51d7a93fbd
Júlio Cesar <juliocesar27072004@outlook.com>
2023-12-18 23:59:34 -0300

    Add files via upload

--- js/Tools/AuxFunctions/populateUlList.js
+++ js/Tools/AuxFunctions/populateUlList.js
@@ -1,33 +1,58 @@
 function populateUlList(ulelement, items, selectedIds = []) {
     ulelement.innerHTML = '';
-
     items.forEach((item) => {
         var listItem = document.createElement("li");
-        var label = document.createElement("label"); // Crie um elemento de rótulo para conter a caixa de seleção e o texto
+        var label = document.createElement("label");
         label.className = "dropdown-item d-flex align-items-center gap-2 py-2";
 
-        var checkbox = document.createElement("input"); // Crie a caixa de seleção
-        checkbox.type = "checkbox"; // Defina o tipo como caixa de seleção
-        checkbox.value = item.id; // Defina o valor como o ID do item
-        checkbox.checked = selectedIds.includes(item.id); // Verifique se o item está selecionado
-
-        // Estilizando a caixa de seleção
-        checkbox.style.marginRight = "5px"; // Margem à direita para separar da legenda
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.value = item.id;
+        checkbox.checked = selectedIds.includes(item.id);
+        checkbox.style.marginRight = "5px";
 
         var anchor = document.createElement("a");
-        // Estilizando o texto dentro do <a> tag
-        anchor.style.color = "black";        // Cor preta
-        anchor.style.textDecoration = "none"; // Remover linha inferior
-        anchor.style.paddingLeft = "5px";    // Padding-left de 5px
-
+        anchor.style.color = "black";
+        anchor.style.textDecoration = "none";
+        anchor.style.paddingLeft = "5px";
         anchor.appendChild(document.createTextNode(item.text));
 
         label.appendChild(checkbox);
         label.appendChild(anchor);
-
         listItem.appendChild(label);
         ulelement.appendChild(listItem);
     });
+
+    // Função para filtrar os itens da lista com base no texto inserido
+    function filterList() {
+        const searchText = document.getElementById('searchInput').value.toLowerCase();
+
+        const listItems = ulelement.getElementsByTagName('li');
+
+        for (let i = 0; i < listItems.length; i++) {
+            const text = listItems[i].innerText.toLowerCase();
+            if (text.includes(searchText)) {
+                listItems[i].style.display = 'block';
+            } else {
+                listItems[i].style.display = 'none';
+            }
+        }
+    }
+
+    // Verifica se o campo de busca já existe antes de criá-lo
+    if (!document.getElementById('searchContainer')) {
+        const searchInput = document.createElement('input');
+        searchInput.setAttribute('type', 'text');
+        searchInput.setAttribute('id', 'searchInput');
+        searchInput.setAttribute('placeholder', 'Buscar por nome...');
+        searchInput.addEventListener('input', filterList);
+
+        const searchContainer = document.createElement('div');
+        searchContainer.setAttribute('id', 'searchContainer');
+        ulelement.parentNode.insertBefore(searchContainer, ulelement);
+        searchContainer.appendChild(searchInput);
+    }
 }
+
 
 export default populateUlList;
